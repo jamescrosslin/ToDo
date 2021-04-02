c784f1c9d619f4150a492eabb682480c18489beb
“bomboloni” <slarge1+github@gmail.com>
2021-04-01 21:41:57 -1000

    index.ejs

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 console.log('hellooooooooo!')
+
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 3000
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todo'
+
+MongoClient.connect(dbConnectionStr, {
+    useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())    
