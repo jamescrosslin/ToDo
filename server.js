89b6c9d7a7a73fc460c1d83891ec5e4258d4bc57
“bomboloni” <slarge1+github@gmail.com>
2021-04-02 09:51:10 -1000

    connecting to database and serving up some html

--- server.js
+++ server.js
@@ -21,3 +21,16 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())    
+
+app.get('/', (request, response) => {
+    db.collection('tasks').find().toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data})
+    })
+    .catch(error => console.error(error))
+})
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
